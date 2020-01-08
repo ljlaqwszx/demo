bd4e9dc2159182cd5f695d27f358d7a3c27ba422
PC-20190922STDI\Administrator <1411225946@qq.com>
2020-01-08 23:47:18 +0800

    demo

---
+++ demo/src/test/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int i = 100;
+		while (i > 0) {
+			new Thread1().run();
+		}
+		
+		
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/Thread1.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Thread1 extends Thread{
+	
+	@Override
+	public void run() {
+		System.out.println(111);
+	}
+
+}
